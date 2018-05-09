07ed5d74b5951b567f6f515837c2457a821b7e99
Sasha Shirokov <lyshirokov613@gmail.com>
2018-05-09 23:58:30 +0300

    start learning comprihensions

--- calc.py
+++ calc.py
@@ -13,7 +13,19 @@ def mult(a, b):
 def div(a, b):
     print(a / b)
 
+
 add(4, 8)
 sub(7, 11)
 mult(5, 8)
 div(10, 2)
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+my_list = []
+for i in nums:
+    my_list.append(i)
+print(my_list[::-1])
+
+my_list = [i for i in nums]
+print(my_list)
