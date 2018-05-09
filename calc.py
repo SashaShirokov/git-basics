561c693d020169e80348e5f19f82271d391f2b15
Sasha Shirokov <lyshirokov613@gmail.com>
2018-05-09 23:48:54 +0300

    added rest arithmetic functions

--- calc.py
+++ calc.py
@@ -2,4 +2,18 @@ def add(a, b):
     print(a + b)
 
 
-add(4,7)
+def sub(a, b):
+    print(a - b)
+
+
+def mult(a, b):
+    print(a * b)
+
+
+def div(a, b):
+    print(a / b)
+
+add(4, 8)
+sub(7, 11)
+mult(5, 8)
+div(10, 2)
