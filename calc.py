fd68636236b68f60d8e01a71a52645b2d6cccec3
Aliaksandr Shyrokau <aliaksandrs@google.com>
2019-09-10 22:01:35 +0300

    keep on learning git

--- calc.py
+++ calc.py
@@ -18,18 +18,3 @@ add(4, 8)
 sub(7, 11)
 mult(5, 8)
 div(10, 2)
-
-
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-my_list = []
-for i in nums:
-    my_list.append(i)
-print(my_list[::-1])
-
-my_list = [i for i in nums]
-print(my_list)
-
-
-l = [n * n for n in nums]
-print(l)
